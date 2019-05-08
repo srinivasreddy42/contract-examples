4e2acda612aa23967451516a943a1d7ed67a1ff6
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2019-05-08 22:22:26 +0530

    Change the hashes as per change in nonce; HelloWorld nonce=0, CounterCall nonce=1, MailingList nonce=2

--- counter/call/src/lib.rs
+++ counter/call/src/lib.rs
@@ -14,8 +14,8 @@ use common::contract_api::pointers::ContractPointer;
 pub extern "C" fn call() {
     //This hash comes from blake2b256( [0;32] ++ [0;8] ++ [0;4] )
     let hash = ContractPointer::Hash([
-        164, 102, 153, 51, 236, 214, 169, 167, 126, 44, 250, 247, 179, 214, 203, 229, 239, 69, 145,
-        25, 5, 153, 113, 55, 255, 188, 176, 201, 7, 4, 42, 100,
+        173, 137, 49, 254, 60, 66, 211, 215, 12, 127, 32, 84, 119, 180, 68, 191, 87, 33,
+        97, 3, 244, 119, 4, 74, 206, 246, 78, 153, 188, 96, 125, 71
     ]);
     let arg = "inc";
     let _result: () = call_contract(hash.clone(), &arg, &Vec::new());
